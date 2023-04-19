63ced8cdc67c7477b9d1445fcc6fffb981ac38ad
theriders <theriders@users.noreply.github.com>
2023-04-19 02:50:18 -0400

    add LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ const char * SSID = "***";
 const char * PASSWORD = "***";
 const byte triggerSonarPin = 12;
 const byte echoSonarPin = 13;
+const byte warningLED = 3;
+const byte cautionLED = 27;
 
 AsyncUDP udp;
 UltraSonicDistanceSensor sonar(triggerSonarPin, echoSonarPin);
@@ -16,6 +18,9 @@ UltraSonicDistanceSensor sonar(triggerSonarPin, echoSonarPin);
 void setup() {
   Serial.begin(9600);
   
+  pinMode(warningLED, OUTPUT);
+  pinMode(cautionLED, OUTPUT);
+
   WiFi.begin(SSID, PASSWORD);
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
         Serial.println("WiFi Failed");
@@ -29,9 +34,10 @@ void setup() {
             Serial.print("Data: ");
             Serial.write(packet.data(), packet.length());
             Serial.print("\n");
-            if(strcmp((char*)packet.data(),"Hello ESP") == 0)
+            if(strcmp((char*)packet.data(),"CAUTION") == 0)
             {
-              Serial.print("WARNING");
+              Serial.print("CAUTION");
+              digitalWrite(cautionLED, HIGH);
             }
         });
     }
@@ -43,7 +49,13 @@ void loop() {
   delay(200);
   //Send broadcast on port 10000
   //udp.broadcastTo("Anyone here?", 10000);
+  // breaking then send message
+  // if have caution light and see car braking turn on WARNING light
   float distance = sonar.measureDistanceCm();
-  //Serial.println(distance);
+  if (0 < distance < 30)
+  {
+    udp.broadcastTo("CAUTION", 10000);
+  }
+  Serial.println(distance);
 }
 
